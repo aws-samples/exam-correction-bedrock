16f39dd0c2bb7e9f6bb02ebac04b425c0ec34d7a
Wembley <wembley@amazon.com>
2024-07-24 11:23:44 -0300

    Jul.24.2 commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dszpj90mbb.execute-api.us-east-1.amazonaws.com/Prod/",
+  baseURL: "API_GATEWAY_URL",
 });
 
 export default api;
